1d9c9a361c286bfe3a3efb0b5a02658d7dccf623
lucasbndv <lucasbndv@gmail.com>
2020-11-14 17:47:45 -0300

    Added to all pages the hamburger

--- src/main/data/scripts/index.js
+++ src/main/data/scripts/index.js
@@ -206,6 +206,21 @@ function eventSetup() {
         }
 
     })
+
+    let menu = document.getElementsByClassName("menu")[0];
+    var menu_on = false;
+    menu.addEventListener("click", () => {
+        let nav = document.getElementById("nav");
+        if (menu_on) {
+            nav.style.display = "none";
+            menu_on = false;
+        }
+        else {
+            nav.style.display = "flex";
+            menu_on = true;
+        }
+
+    })
 }
 
 main();
