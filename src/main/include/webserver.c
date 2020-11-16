274ad155811def0e064e6c764575c9c0a1458717
Igor Kelvin <igorkelvin@gmail.com>
2020-11-16 13:41:58 -0300

    Config + database working

--- src/main/include/webserver.c
+++ src/main/include/webserver.c
@@ -1,127 +1,90 @@
 #include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
-#include <sqlite3.h>
 
-WebServer server(80);
+#include <sqlite3.h>
+#include <Arduino_JSON.h>
+
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+
+#include "JSONHandler.h"
+
+JSONHandler* json_handler;
+
+AsyncWebServer server(80);
+
 sqlite3 *db;
 
-void handleNotFound()
+void handleNotFound(AsyncWebServerRequest * request)
 {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for (uint8_t i = 0; i < server.args(); i++)
-  {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-
-  server.send(404, "text/plain", message);
+  request->send(SPIFFS, "/index.html", "text/html");
 }
 
-void handleRoot()
+void handleRoot(AsyncWebServerRequest * request)
 {
-  char temp[400];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
 
-  float temperature = BMP280ReadTemperature();
-  float pressure = BMP280ReadPressure();
-  float altitude = BMP280ReadAltitude();
-
-  Serial.println(temperature);
-
-  snprintf(temp, 400,
-#include "html.h"
-           , hr, min % 60, sec % 60,
-           temperature, pressure, altitude);
-
-  server.send(200, "text/html", temp);
 }
 
-void handleAPI()
+void handleAPI(AsyncWebServerRequest * request)
 {
-  char msg[610] = "%.02f,%.02f,%.02f";
-  Serial.println("API");
-  snprintf(msg, sizeof(msg), msg, BMP280ReadTemperature(), BMP280ReadPressure(), BMP280ReadAltitude());
-  Serial.println(msg);
-  server.send(200, "text/plain", msg);
+  //char msg[610] = "%.02f,%.02f,%.02f";
+  //Serial.println("API");
+  //snprintf(msg, sizeof(msg), msg, BMP280ReadTemperature(), BMP280ReadPressure(), BMP280ReadAltitude());
+  //Serial.println(msg);
+  //request->send(200, "text/plain", msg);
 }
 
-void handleDashboard()
+void handleDashboard(AsyncWebServerRequest * request)
 {
   char *zErrMsg = 0;
   int rc;
   sqlite3_stmt *res;
   const char *tail;
-  const char *data = "Callback function called";
-  char *sql = "SELECT * FROM museu ORDER BY rowid DESC LIMIT 10 ";
+  const char data[] = "Callback function called";
+  
+  char sql[] = "SELECT * FROM museu ORDER BY rowid DESC LIMIT 10 ";
   rc = sqlite3_prepare_v2(db, sql, strlen(sql), &res, &tail);
+  
   if (rc != SQLITE_OK)
   {
     String resp = "Failed to fetch data: ";
     resp += sqlite3_errmsg(db);
     sqlite3_close(db);
-    Serial.println(resp.c_str());
+    Serial.println(resp);
+    
     return;
   }
 
-  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
-  String resp = "<html><head><title>Estação Ambiental Museu</title>\
-          <meta http-equiv='refresh' content='5' />\
-          <meta charset='utf-8'/>\
-          <style>\
-          body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; font-size: large; Color: #000088; }\
-          #table{width:300px,height:100px}\
-          </style><head><body><h1>Estação Ambiental Museu</h1>";
-
-  resp += "<br><table cellspacing='1' cellpadding='1' border='1' id='table'><tr><td>Data<td>Temperatura</td><td>Humidade</td><td>Pressão</td><td>Luminosidade</td><td>Co2</td><td>Poeira</td></tr>";
-  server.send(200, "text/html", resp.c_str());
+  JSONVar response;
+  int i = 0;
   while (sqlite3_step(res) == SQLITE_ROW)
   {
-    resp = "<tr><td>";
-    resp += (const char *)sqlite3_column_text(res, 0);
-    resp += "</td><td>";
-    resp += sqlite3_column_double(res, 1);
-    resp += "</td><td>";
-    resp += sqlite3_column_double(res, 2);
-    resp += "</td><td>";
-    resp += sqlite3_column_double(res, 3);
-    resp += "</td><td>";
-    resp += sqlite3_column_double(res, 4);
-    resp += "</td><td>";
-    resp += sqlite3_column_double(res, 5);
-    resp += "</td><td>";
-    resp += sqlite3_column_double(res, 6);
-    resp += "</td></tr>";
-
-    server.sendContent(resp);
+    response[i]["Hora_UTC"]     = (const char *)sqlite3_column_text(res, 0);
+    response[i]["Temperatura"]  = sqlite3_column_double(res, 1);
+    response[i]["Humidade"]     = sqlite3_column_double(res, 2);
+    response[i]["Pressão"]      = sqlite3_column_double(res, 3);
+    response[i]["Luminosidade"] = sqlite3_column_double(res, 4);
+    response[i]["Co2"]          = sqlite3_column_double(res, 5);
+    response[i]["Poeira"]       = sqlite3_column_double(res, 6);
   }
+  
   sqlite3_finalize(res);
+  request->send(200, "application/json", JSON.stringify(response));
 }
 
 void webServerSetup(sqlite3 *db1)
 {
-  db = db1;
-  if (MDNS.begin("esp32"))
-  {
-    Serial.println("MDNS responder started");
-  }
+  //if (MDNS.begin("esp32"))
+  //{
+    //Serial.println("MDNS responder started");
+  //}
 
-  server.on("/", handleRoot);
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-  server.on("/getdata", handleAPI);
+  server.serveStatic("/", SPIFFS, "/");
+  
+  //server.on("/getdata", handleAPI);
   server.on("/dashboard", handleDashboard);
+  
   server.onNotFound(handleNotFound);
   server.begin();
+  
   Serial.println("HTTP server started");
 }
--- src/main/main.ino
+++ src/main/main.ino
@@ -1,84 +1,24 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
-#include <sqlite3.h>
-#include <SPI.h>
-#include <FS.h>
-#include "SD.h"
+#include "include/config.c"
+#include "include/database.c"
 
-#include "include/BMP280.c"
-#include "include/webserver.c"
-#include "include/Database.c"
-#include "include/rtc.c"
 
-const char *ssid = "DNAModas";
-const char *password = "mengao81";
+void setup() 
+{
 
-sqlite3 *db1;
-int timer = 0;
-
-void setup() {
-
- 
-  int rc;
-  sqlite3_stmt *res;
-  int rec_count = 0;
-  const char *tail;
-
-  Serial.begin(9600);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  SPI.begin();
-  SD.begin();
-  sqlite3_initialize();
-
-  // Open database
-  if(openDb("/sd/data/museu.db", &db1)){
-    return;
-  }
+  Serial.begin(115200);
   
-  //dbClear(db); // Delete table museu
-  dbSetup(db1);
-  //rtcSetup();
-  BMP280Setup();
-  webServerSetup(db1);
-  //readDB(db1);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  server.handleClient();
-  if (millis() - timer > 2000){
-    saveData();
-    timer = millis();
+  if (config("/config/config.cfg")) 
+  {
+    config_set_default();
   }
+
+  delay(2000);
+  database_setup();
 }
 
-void saveData(){
-  //String date_iso = rtcGetDate();
-  String date_iso = "09/11/2020T12:58";
-  float temperature = BMP280ReadTemperature();
-  float humidity = BMP280ReadHumidity();
-  float pressure = BMP280ReadPressure();
-  float luminosity = analogRead(34);
-  float co = 0.0;
-  float dust = 0.0;
-  float datalist[6] = {temperature, humidity, pressure, luminosity, co, dust};
-  writeOnDB(date_iso.c_str(),datalist,db1);
+void loop() 
+{
+  database_save_data();
+
+  delay(5000);
 }
